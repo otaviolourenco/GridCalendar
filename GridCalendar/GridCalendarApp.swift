9a2dc902819e87542f5a8b4c6020992171b4f0c4
Otávio Lourenço <otavioluissv@gmail.com>
2024-11-14 11:25:26 +0000

    Initial commit

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Otavio Lourenço on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
